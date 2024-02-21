405b42e033b08045acad5da0d5767417126f3356
Vinicius <vfarias.designer@gmail.com>
2024-02-20 21:10:25 -0300

    finalized app

--- js/app.js
+++ js/app.js
@@ -1,19 +1,34 @@
 import { getCityData, getCityWeather } from "./weather.js";
 
+const temperature = document.querySelector('.hero-temperature');
+const statusClimate = document.querySelector('.status-climate');
+const timeIcon = document.querySelector('.hero-icon');
+
+
 const latitude = localStorage.getItem('latitude');
 const longitude = localStorage.getItem('longitude');
 
-
 const fetchCityWeatherInfo = async () => {
-    const {Key} = await getCityData(latitude, latitude);
+    if(!latitude && !longitude){
+        alert('disponibilize sua localização para a aplicação funcionar corretamente');
+        return;
+    }
+    const {Key} = await getCityData(latitude, longitude);
     const [{Temperature, WeatherText, WeatherIcon}] = await getCityWeather(Key)
-    const obj = await getCityWeather(Key)
-    console.log(obj);
-    return;
+    
+    return {Temperature, WeatherText, WeatherIcon};
+}
+
+const injectHtml = async () => {
+    const {Temperature, WeatherText, WeatherIcon} = await fetchCityWeatherInfo();
+    temperature.innerText = Temperature.Metric.Value;
+    statusClimate.innerText = WeatherText;
+    timeIcon.setAttribute('src', `assets/icons/${WeatherIcon}.svg`);
 }
 
 export const weatherApp = () => {
-    fetchCityWeatherInfo(latitude, longitude);
+    // fetchCityWeatherInfo();
     // const obj = await getCityData(latitude, longitude);
     // console.log(obj)
+    injectHtml();
 }
